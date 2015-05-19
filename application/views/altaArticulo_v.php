eec80b03f2aaffa6e39f7e3dbe935b6f7eb2325d
vish45 <osskar_the_pimp@hotmail.com>
2015-05-19 12:15:24 -0500

    Se modificaron nuevamente las vistas y se corrigieron varios bugs en tiempos de carga

--- application/views/altaArticulo_v.php
+++ application/views/altaArticulo_v.php
@@ -1,48 +1,47 @@
-<<<<<<< HEAD
-<div class="container col-xs-12 col-sm-12 col-md-9 col-lg-9">
-	<center>
-		<form class="center" action="" method="POST">
-			<h2>Alta Articulo.</h2>
-
-			<label> Tipo: </label>
-			<select id="id_tipo_mueble">
-				<option value="0">Seleccionar tipo</option>
-				<?php foreach ($cat_tipo as $key => $value) { ?>
-					<option value="<?php echo $key;?>"><?php echo $value['descripcion'];?></option>
-				<?php }?>
-				<option onclick="divFormTipo()">Otro...</option>
-			</select>
-			<br><br>
-			<label> Progresivo: </label>
-			<input type="text" class="form-control" name="txtProgresivo" id="txtProgresivo" placeholder="Ingrese progresivo">
-			<br><br>
-			<label> Caracteristicas: </label>
-			<input type="text" class="form-control" name="txtCaracterisicas" id="txtCaracterisicas" placeholder="Caracteristicas del articulo">
-			<br><br>
-			<label> Estatus: </label>
-			<select id="id_estatus">
-				<option value="0">Seleccionar estatus</option>
-				<?php foreach ($cat_estatus as $key => $value) { ?>
-					<option value="<?php echo $key;?>"><?php echo $value['descripcion'];?></option>
-				<?php }?>
-			</select>
-			<br><br>
-			<button type="submit" class="btn btn-default">Cancelar</button>
-			<button id="btnGuardar" onClick="guardarArticulo()" class="btn btn-default">Guardar</button>
-
-		</form><!--FinFormAltaArt-->
-	</center>
-</div><!--FinDivAltaArt-->
-
-<!--Scripts-->
-<script type="text/javascript" 	src = " <?php echo base_url(); ?>statics/js/jquery.js"></script>
-<script type="text/javascript" 	src = " <?php echo base_url(); ?>statics/js/altaArticulo.js"></script>
-
-=======
-<div class="container">
-	<form>		
-	  <h2 class="text-center">Alta artículos.</h2>
-		
-	</form>
-</div> <!--DinDivConteiner-->
->>>>>>> b3c37f0963326fb2e903b4565f1d6b6435572d4f
+	<!-- MODAL DE ALTA DE USUARIO -->
+		<form action="" method="POST" role ="form">
+				<div class="col-xs-12 col-sm-12 col-md-9 col-lg-9">
+					<div class="form-group"> 
+					<br><strong><center><legend>Formulario Alta Articulo</legend></center></strong>
+						<label> Tipo: </label>
+							<select class = "form-control" id="id_tipo_mueble">
+								<option value="0">Seleccionar tipo</option>
+									<?php foreach ($cat_tipo as $key => $value) { ?>
+									<option value="<?php echo $key;?>"><?php echo $value['descripcion'];?></option>
+									<?php }?>
+								<option onclick="divFormTipo()">Otro...</option>
+							</select>
+					</div>
+					<br>
+					
+					<div class="form-group">
+						<label> Progresivo: </label>
+						<input type="text" class="form-control" name="txtProgresivo" id="txtProgresivo" placeholder="Ingrese progresivo">
+					<br>
+					</div>
+				
+					<div class="form-group">
+						<label> Caracteristicas: </label>
+						<input type="text" class="form-control" name="txtCaracterisicas" id="txtCaracterisicas" placeholder="Caracteristicas del articulo">
+					<br>
+					</div>
+					
+					<div class="form-group">
+						<label> Estatus: </label>
+					       	<select class = "form-control" id="id_estatus">
+								<option value="0">Seleccionar estatus</option>
+								<?php foreach ($cat_estatus as $key => $value) { ?>
+								<option value="<?php echo $key;?>"><?php echo $value['descripcion'];?></option>
+								<?php }?>
+							</select>
+					</div>
+			</div>
+				
+				<div class="modal-footer">
+					<button type="reset" class="btn btn-default" data-dismiss="modal">Cancelar</button>
+					<button type="button"  onClick="guardarArticulo()" class="btn btn-primary" >Guardar</button>
+				</div>
+				
+				</div></form>
+			</div><!-- /.modal-content -->
+		</div><!-- /.modal-dialog -->
