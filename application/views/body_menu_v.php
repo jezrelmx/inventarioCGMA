08bb55f5ec8863bbf3ea76ef5fdb48b6cdabbd50
vish45 <osskar_the_pimp@hotmail.com>
2015-05-20 13:57:50 -0500

    Se corrigio la tabla

--- application/views/body_menu_v.php
+++ application/views/body_menu_v.php
@@ -1,8 +1,8 @@
 <body>
 <header id="header" class="">
 	<div>
-		<img src="<?php echo base_url();?>statics/img/om-gray.png" width="70" heigth="70"  style="float:left"/>
 		<img src="<?php echo base_url();?>statics/img/ciudad-de-mexico-logo-gray.png" width="70" heigth="70"  style="float:left"/>
+		<img src="<?php echo base_url();?>statics/img/om-gray.png" width="70" heigth="70"  style="float:left"/>
 		<lable id = "fuente"  style="float:left">Sistema De Inventario CGMA</label>
 	</div>
 	<div style="clear:both"></div>
