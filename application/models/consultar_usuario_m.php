8b484d724d1b5aa19f490a56754842a163fb85f7
unknown <marynarcigmail.com>
2015-05-20 17:48:55 -0500

    Se modifico el modelo para consultar usuario por identificador, de tal modo que no regresa el pass y te mande el tipo de usuario del catálogo correspondiente

--- application/models/consultar_usuario_m.php
+++ application/models/consultar_usuario_m.php
@@ -9,14 +9,19 @@ class Consultar_usuario_m extends CI_Model {
 	
 	public function obtener_usuario($id=0){
 		if($id==0){
-			$customQuery="select u.*, c_t_d.nombre as nom_direccion from usuarios u 
-					inner join cat_direccion_ejecutiva c_t_d on(c_t_d.id_direccion_ejecutiva=u.id_direccion_ejecutiva) 
-					where estatus=1 order by id_usuario ";
+			$customQuery="select c_t_u.descripcion as tipo_usuario, u.id_usuario, u.nombre, u.ap_paterno, u.ap_materno, u.cargo, u.num_empleado, 
+      		 c_d_t.nombre as nom_direccion,u.id_direccion_ejecutiva, u.id_tipo_usuario, 
+      		 u.email, u.estatus from usuarios u
+      		 inner join cat_tipo_usuario c_t_u on (c_t_u.id_tipo_usuario=u.id_tipo_usuario)
+      		 inner join cat_direccion_ejecutiva c_d_t on(u.id_direccion_ejecutiva=c_d_t.id_direccion_ejecutiva) order by id_usuario;";
 			
 		}else{
-			$customQuery="select u.*, c_t_d.nombre as nom_direccion from usuarios u 
-					inner join cat_direccion_ejecutiva c_t_d on(c_t_d.id_direccion_ejecutiva=u.id_direccion_ejecutiva) 
-					where id_usuario=".$id." and estatus=1 order by id_usuario;";
+			$customQuery="select c_t_u.descripcion as tipo_usuario, u.id_usuario, u.nombre, u.ap_paterno, u.ap_materno, u.cargo, u.num_empleado, 
+      		 c_d_t.nombre as nom_direccion,u.id_direccion_ejecutiva, u.id_tipo_usuario, 
+      		 u.email, u.estatus from usuarios u
+      		 inner join cat_tipo_usuario c_t_u on (c_t_u.id_tipo_usuario=u.id_tipo_usuario)
+      		 inner join cat_direccion_ejecutiva c_d_t on(u.id_direccion_ejecutiva=c_d_t.id_direccion_ejecutiva)
+			 where id_usuario=".$id." and estatus=1 order by id_usuario;";
 		}
 		
 		$resultado = $this->db->query($customQuery);	
